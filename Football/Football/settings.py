383a7cf5fccf0dc5ccd60462d54c882d007d8d03
HemantDimri27 <dimrihemant27@gmail.com>
2025-01-02 11:14:30 +0530

    project setup with an app(tweet)

--- Football/Football/settings.py
+++ Football/Football/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tweet',                            # make aware about new app
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'Football.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],                  # add template dir
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
